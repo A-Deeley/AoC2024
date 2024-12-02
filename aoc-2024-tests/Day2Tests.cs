4c83ecb42a6366f8f5e16ef180487ddca3e1314d
Deeley Andrew <andrew.deeley@cqsb.qc.ca>
2024-12-02 12:18:04 -0500

    Init day 2

---
+++ aoc-2024-tests/Day2Tests.cs
@@ -0,0 +1,25 @@
+﻿using aoc_2024.IO;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_2024_tests;
+
+public class Day2Tests
+{
+    readonly IPuzzleInputReader _inputReader;
+    readonly string sample =
+        """
+        
+        """;
+
+    public Day2Tests()
+    {
+        _inputReader = Substitute.For<IPuzzleInputReader>();
+        _inputReader.GetPuzzleInput(2).Returns(sample);
+    }
+
+}
